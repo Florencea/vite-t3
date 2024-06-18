58db2fc98c9491f6753806bd134688e622dd8592
Florencea <bearflorencea@gmail.com>
2024-06-18 12:52:15 +0800

    fix: remove eslint react

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
@@ -35,13 +34,10 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "error",
